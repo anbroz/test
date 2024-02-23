05094af2c3a9e87cc84932c7640868e80c138c4d
anbroz <153028301+anbroz@users.noreply.github.com>
2024-02-23 14:45:22 +0100

    little bug fixed

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
+
 
 function kopiraj() {
-    $('#box-kopirano').hide();
     navigator.clipboard.writeText("windows_10_");
-    $('#box-kopirano').show();
-    sleep(2000).then(() => { $('#box-kopirano').hide();; });
-    
+    alert("copyed!")
 }
