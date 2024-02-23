7d2fcf6d2d3e8eb105a921c7561b80897193ce4b
anbroz <153028301+anbroz@users.noreply.github.com>
2024-02-23 22:23:29 +0100

    mini-update_2

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+function navigacija_discord_pokatiHelpBox() {
+    var x = document.getElementById("box-kopirano");
+    x.style.display = "block";
+
+    setTimeout(function() {
+        x.style.display = "none";
+    }, 2000);
+}
 
 
-function kopiraj() {
+
+
+function navigacija_discord_kopiraj() {
     navigator.clipboard.writeText("windows_10_");
-    alert("copyed!")
+    
 }
